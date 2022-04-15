091019fd665210a5131c0c71f17250d72408dd0a
Ayush Kaushik <ayushkaushik@Ayushs-MacBook-Pro.local>
2022-04-15 12:10:41 +0530

    15 Apr

---
+++ Q669.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(!root)return NULL;
+        if(root->val<low){
+            root=trimBST(root->right,low,high);
+        }
+        else if(root->val>high)
+        {
+            root=trimBST(root->left,low,high);
+        }
+        else
+        {
+            root->left=trimBST(root->left,low,high);
+            root->right=trimBST(root->right,low,high);
+        }
+        return root;
+    }
+};
